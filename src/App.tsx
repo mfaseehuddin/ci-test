3c5191fae5c7eaebc678f47b99446ca788e6a5ec
Muhammad Faseeh Ud Din <mfaseehuddin2001@gmail.com>
2022-12-07 01:03:10 +0500

    Initial Commit: Testing CI/CD FTP Deploy, with github actions

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testing CI/CD for FTP Deployment with react</h1>
+      <h2>building app using server commands on github actions</h2>
+    </div>
+  );
+}
+
+export default App;
